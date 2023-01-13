b9e37b9bcea6bf218ea5cc4b3a2e85442a1e735b
Viktor-117 <viktor.hushchin@gmail.com>
2023-01-13 13:31:01 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import { Route, Routes } from 'react-router-dom';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
     </div>
   );
-};
+}
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,9 @@
+import { Box, Header } from './AppHeader.styled';
+
+export function AppHeader() {
+  return (
+    <Box>
+      <Header>Weather forecast</Header>
+    </Box>
+  );
+}
